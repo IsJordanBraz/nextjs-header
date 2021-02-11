9872c4ea56c3a5c08b881ee57e714dd81482c622
Jordan <jordanbraz@outlook.com>
2021-02-11 20:12:41 -0300

    Babel change from react to next

--- src/Header.js
+++ src/Header.js
@@ -1,7 +1,14 @@
+import Image from 'next/image'
 const Header = () => {
   return (
     <div>
       <h3>Minha Library NextJs</h3>
+      <Image
+        src="https://miro.medium.com/max/1200/1*mk1-6aYaf_Bes1E3Imhc0A.jpeg"
+        alt="Picture of the author"
+        width={500}
+        height={500}
+      />
     </div>
   )
 }
