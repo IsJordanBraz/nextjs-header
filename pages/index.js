a08e58f28c9172cafe4d83ecccf4aac2cf3c3595
Jordan <jordanbraz@outlook.com>
2021-02-11 14:26:45 -0300

    NextJs Library Test

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import Header from "../src";
+
+function HomePage() {
+  return <div>
+    Welcome to Next.js!
+    <Header />
+  </div>
+}
+
+export default HomePage
---
+++ src/Header.js
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <div>
+      <h3>Minha Library NextJs</h3>
+    </div>
+  )
+}
+
+export default Header;
